de35c642ab988f60ed8e9610d46ca8d70000695d
EthanRGraham <ethan.graham@bsu.edu>
2025-09-10 17:44:12 +0000

    practice

---
+++ cpp_practice.cpp
@@ -0,0 +1,18 @@
+/* practicing cpp
+ *
+ *
+ *
+ */
+
+#include <iostream>
+
+int main(){
+	std::string username;
+
+	std::cout << "Testing, testing...Hello! Would you please tell me your name?";
+	std::cin >> username;
+
+	std::cout << "Thank you." << username << "We are figuring things out, I appreciate your participation." << std::endl;
+
+	return 0;
+} // end main
