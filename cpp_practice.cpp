4d12f01d0c684caf1b6ed2a03313b5ff4dcb49e8
EthanRGraham <ethan.graham@bsu.edu>
2025-09-10 17:58:16 +0000

    updated some formatting

--- cpp_practice.cpp
+++ cpp_practice.cpp
@@ -12,7 +12,7 @@ int main(){
 	std::cout << "Testing, testing...Hello! Would you please tell me your name?";
 	std::cin >> username;
 
-	std::cout << "Thank you." << username << "We are figuring things out, I appreciate your participation." << std::endl;
+	std::cout << "Thank you " << username << ". We are figuring things out, I appreciate your participation." << std::endl;
 
 	return 0;
 } // end main
